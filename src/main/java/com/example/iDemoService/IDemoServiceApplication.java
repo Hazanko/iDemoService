9a1dcc751b193a06c0a6cd111ba8128d24facce3
Hazanko <joevankollenburg@hotmail.com>
2025-06-25 14:38:23 +0200

    Initial commit - spring boot initializer

---
+++ src/main/java/com/example/iDemoService/IDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.iDemoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IDemoServiceApplication.class, args);
+	}
+
+}
